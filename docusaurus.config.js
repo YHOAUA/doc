9d4e7cc71e197836373ea2000d3e99c6cd3f39a2
伊霍安 <yhoauann@gmail.com>
2024-12-10 21:48:24 +0800

    add

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,12 +10,12 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '网络记事本',
+  title: 'Doc',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ydoc.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -46,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/YHOAUA/doc/tree/master/',
         },
         blog: {
           showReadingTime: true,
